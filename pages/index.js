4bba7de81b1ed8b85b7886702b681a3b071f7d84
Soft Bucket IO <softbucket.io@gmail.com>
2021-06-19 11:52:20 +0300

    changed from layout to Layout

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Layout from '../components/layout';
+import Layout from '../components/Layout';
 import { useRouter } from 'next/router';
 import { usePaginateJobs } from '../utils/useRequest';
 import HomeJobList from '../components/HomeJobList';
