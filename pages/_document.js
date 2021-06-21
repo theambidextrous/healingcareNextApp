3b8152553b0f80fc66b2d5c0592352dc2d8d43be
Soft Bucket IO <softbucket.io@gmail.com>
2021-06-21 12:48:56 +0300

    added google trucking code

--- pages/_document.js
+++ pages/_document.js
@@ -36,6 +36,17 @@ export default class HTDocument extends Document {
                     </noscript> */}
 
                     <link className="skin" rel="stylesheet" type="text/css" href="/static/assets/template/css/skin/skin-1.css" />
+                    <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+                    <script
+                        dangerouslySetInnerHTML={{
+                        __html: `
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                        page_path: window.location.pathname,
+                        });
+                    `,}}/>
                 </Head>
                 <body id="bg">
                     <Main />
